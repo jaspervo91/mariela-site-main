cc402e19bbc21a8da6ba1f8adee121ccd502c0a0
jaspervo91 <jaspervanouwerkerk@gmail.com>
2024-02-24 13:31:30 +0100

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,12 @@
 module.exports = (config) => {
+  // Tell 11ty to use the .eleventyignore and ignore our .gitignore file
+  config.setUseGitIgnore(false);
+
+  // Set directories to pass through to the dist folder
+  config.addPassthroughCopy("./src/fonts");
+  config.addPassthroughCopy("./src/images/");
+  config.addPassthroughCopy("./src/css/");
+
   return {
     markdownTemplateEngine: "njk",
     dataTemplateEngine: "njk",
